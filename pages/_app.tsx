28a97491e7f2188102d5a3ac6dc56c02da61a756
Reggie P <enso@pm.me>
2023-01-17 22:31:31 -0500

    initial build & auth0 integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import type { AppProps } from 'next/app'
+import React from 'react';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps } : AppProps) => {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
+
+export default App
